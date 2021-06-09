27b770e21bd63ab2b16031a90b1ab72a124aabf3
dc2 <dc2@demandcluster.com>
2021-06-09 10:40:05 +0200

    chore: prepare

--- prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm ./imports/plugins/custom/reaction-demandcluster -R
-git clone https://DemandDevOps:$GITHUB_TOKEN@github.com/demandcluster/reaction-demandcluster.git ./imports/plugins/custom/reaction-demandcluster
-././imports/plugins/custom/reaction-demandcluster/npm install
-
-
