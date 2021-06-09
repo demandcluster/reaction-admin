e4ff67312b3feaed5e78a6fbb1accd7d825314fd
dc2 <dc2@demandcluster.com>
2021-06-09 08:41:09 +0200

    chore: github token

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm ./imports/plugins/custom/reaction-demandcluster -R
-git clone https://demandcluster:$GH_TOKEN@github.com/demandcluster/reaction-demandcluster.git ./imports/plugins/custom/reaction-demandcluster
+git clone https://demandcluster:$GITHUB_TOKEN@github.com/demandcluster/reaction-demandcluster.git ./imports/plugins/custom/reaction-demandcluster
 ././imports/plugins/custom/reaction-demandcluster/npm install
 
 
