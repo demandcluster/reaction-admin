42d8c0ebb41c30caf1be4849285a229387fa351b
dc2 <dc2@demandcluster.com>
2021-07-12 14:25:56 +0200

    chore: update

--- imports/plugins/core/templates/client/components/EmailTemplateForm.js
+++ imports/plugins/core/templates/client/components/EmailTemplateForm.js
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import i18next from "i18next";
 import { useMutation } from "@apollo/react-hooks";
 import { useSnackbar } from "notistack";
 import SimpleSchema from "simpl-schema";
 import { Button, TextField } from "@reactioncommerce/catalyst";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Grid,
-  makeStyles
-} from "@material-ui/core";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Grid, makeStyles } from "@material-ui/core";
 import muiOptions from "reacto-form/cjs/muiOptions";
 import useReactoForm from "reacto-form/cjs/useReactoForm";
 import updateEmailTemplateGQL from "../graphql/mutations/updateEmailTemplate";
+
+import { EditorState, convertToRaw, ContentState, createFromRaw, convertFromHTML } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const useStyles = makeStyles((theme) => ({
   deleteButton: {
@@ -47,6 +46,33 @@ const emailTemplateSchema = new SimpleSchema({
 });
 const validator = emailTemplateSchema.getFormValidator();
 
+function myBlockRenderer(contentBlock) {
+  const type = contentBlock.getType();
+
+  // 图片类型转换为mediaComponent
+  if (type === "atomic") {
+    return {
+      component: Editor,
+      editable: false,
+      props: {
+        foo: "bar"
+      }
+    };
+  }
+}
+
+const getTemplate = (html) => {
+  //const html = emailTemplate.template;
+  //const html = "<p>Hey this <strong>editor</strong> rocks 😀</p>";
+
+  const blocksFromHtml = convertFromHTML(html);
+
+  const { contentBlocks, entityMap } = blocksFromHtml;
+  const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
+  // const editorState = EditorState.createWithContent(contentState);
+  return contentState;
+};
+
 /**
  * @summary React component that renders the form for updating an e-mail template record.
  * @param {Object} props React props
@@ -55,8 +81,29 @@ const validator = emailTemplateSchema.getFormValidator();
 export default function EmailTemplateForm(props) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
+  //const defaultTemplate = setTemplate();
 
   const { emailTemplate, isOpen, onCloseDialog, refetch, shopId } = props;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [template, setTemplate] = useState("");
+
+  useEffect(() => {
+    if (props.emailTemplate && props.emailTemplate.template !== template) {
+      setTemplate(props.emailTemplate.template);
+
+      let bodyHtml = /<body.*?>([\s\S]*)<\/body>/.exec(props.emailTemplate.template)[1];
+      bodyHtml = bodyHtml.replaceAll(/\<\!--.*-->/g, "");
+      //  bodyHtml = bodyHtml.replaceAll(/{{.*}}/g, "");
+      const finalHtml = `<body>${bodyHtml}</body>`;
+      //console.log(finalHtml);
+      const templateDraft = getTemplate(finalHtml);
+      console.log("drafting", templateDraft);
+
+      setEditorState(EditorState.createWithContent(templateDraft));
+    }
+  }, [props.emailTemplate]);
+
+  const editor = useRef(null);
 
   const onSuccess = () => {
     setIsSubmitting(false);
@@ -81,19 +128,13 @@ export default function EmailTemplateForm(props) {
     }
   });
 
-  const {
-    getFirstErrorMessage,
-    getInputProps,
-    isDirty,
-    hasErrors,
-    submitForm
-  } = useReactoForm({
+  const { getFirstErrorMessage, getInputProps, isDirty, hasErrors, submitForm } = useReactoForm({
     async onSubmit(formData) {
       setIsSubmitting(true);
 
       if (emailTemplate) {
         const emailTemplateInput = emailTemplateSchema.clean(formData);
-
+        setEditorState({ emailTemplateInput });
         await updateEmailTemplate({
           variables: {
             input: {
@@ -115,6 +156,10 @@ export default function EmailTemplateForm(props) {
   });
 
   const classes = useStyles();
+
+  const onBlockChange = (state) => {
+    setEditorState(state);
+  };
 
   return (
     <div>
@@ -158,14 +203,20 @@ export default function EmailTemplateForm(props) {
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
-                error={hasErrors(["template"])}
-                fullWidth
-                helperText={getFirstErrorMessage(["template"])}
-                label={i18next.t("templateGrid.columns.template")}
-                placeholder={i18next.t("templateUpdateForm.emailTemplates.placeholder.template")}
-                multiline
-                {...getInputProps("template", muiOptions)}
+              <Editor
+                ref={editor}
+                editorState={editorState}
+                onEditorStateChange={onBlockChange}
+                blockRendererFn={myBlockRenderer}
+                {...getInputProps("template", {
+                  nullValue: "",
+                  propNames: {
+                    errors: false,
+                    hasBeenValidated: false,
+                    isReadOnly: "disabled",
+                    onSubmit: false
+                  }
+                })}
               />
             </Grid>
           </Grid>
@@ -174,12 +225,7 @@ export default function EmailTemplateForm(props) {
           <Button variant="outlined" onClick={onCloseDialog} color="primary">
             {i18next.t("app.cancel")}
           </Button>
-          <Button
-            disabled={isSubmitting || !isDirty}
-            onClick={submitForm}
-            variant="contained"
-            color="primary"
-          >
+          <Button disabled={isSubmitting || !isDirty} onClick={submitForm} variant="contained" color="primary">
             {i18next.t("app.save")}
           </Button>
         </DialogActions>
